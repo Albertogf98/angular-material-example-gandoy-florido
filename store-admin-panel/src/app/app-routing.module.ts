cf0ccb3f5b5914571b5ccc3da64b823bb07d5e71
Albertogf98 <alberto1998adoratrices@gmail.com>
2021-01-20 13:40:12 +0100

    master(dashboad) create /dashboard

--- store-admin-panel/src/app/app-routing.module.ts
+++ store-admin-panel/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { DashComponent } from './dash/dash.component';
+
+const routes: Routes = [{ path: 'dashboard', component: DashComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
